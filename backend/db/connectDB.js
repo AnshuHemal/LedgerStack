bdfd2a1ff57a20e692086e660c734e914d1d5ea9
Mr. AnshuHemu <anshukatariya4488@gmail.com>
2025-08-31 17:53:25 +0530

    Vercel Backend File..

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async() => {
   try {
-    const connection = await mongoose.connect(process.env.MONGO_URL)
+    const connection = await mongoose.connect("mongodb+srv://whiteturtle1:dvIw4evFuDVOzea3@cluster0.1e4vx.mongodb.net/ledgerstack?retryWrites=true&w=majority&appName=Cluster0")
     console.log(`Server connected to Database..`)
   } catch (error) {
     console.log(`Error connecting to database: ${error.message}`)
