11fef0c0825821a8411a9bbd9ff2c95b089f82e4
Mr. AnshuHemu <anshukatariya4488@gmail.com>
2025-08-31 17:49:56 +0530

    Vercel Backend File..

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://ledgerstack.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
